88660344dcabea2e8e5d790e34a32eb5807cff32
cookie0010 <sg.been2017@gmail.com>
2023-07-15 03:18:47 +0900

    feat: recipe fetch utils

--- src/lib/utils/api/plants/getPlants.ts
+++ src/lib/utils/api/plants/getPlants.ts
@@ -1,6 +1,7 @@
 import API_BASE_URL from '@/lib/constants/api';
+import { PlantRequest, PlantResponse } from '@/lib/types/api';
 
-const getPlants = async (plantId: number) => {
+const getPlants = async ({ plantId }: PlantRequest): Promise<PlantResponse> => {
   const response = await fetch(`${API_BASE_URL}/plants`, {
     method: 'GET',
     body: JSON.stringify({
@@ -8,11 +9,12 @@ const getPlants = async (plantId: number) => {
     }),
   });
 
+  const responseData: PlantResponse = await response.json();
   if (!response.ok) {
-    console.log(response.statusText);
+    throw new Error('식물 정보를 불러오는데 실패했습니다.');
   }
 
-  return response;
+  return responseData;
 };
 
 export default getPlants;
--- src/lib/utils/api/recipe/getRecipe.ts
+++ src/lib/utils/api/recipe/getRecipe.ts
@@ -1,15 +1,17 @@
-// import API_BASE_URL from '@/lib/constants/api';
-//
-// const getRecipe = async () => {
-//   const response = await fetch(`${API_BASE_URL}/recipe`, {
-//     method: 'GET',
-//   });
-//
-//   if (!response.ok) {
-//     console.log(response.statusText);
-//   }
-//
-//   return response;
-// };
-//
-// export default getRecipe;
+import API_BASE_URL from '@/lib/constants/api';
+import { RecipeResponse } from '@/lib/types/api';
+
+const getRecipe = async (): Promise<RecipeResponse> => {
+  const response = await fetch(`${API_BASE_URL}/recipe`, {
+    method: 'GET',
+  });
+
+  const responseData: RecipeResponse = await response.json();
+  if (!response.ok) {
+    throw new Error('레시피 정보를 불러오는데 실패했습니다.');
+  }
+
+  return responseData;
+};
+
+export default getRecipe;
