d0c3d26b7966cce64f94a0612664880be645ca90
cookie0010 <sg.been2017@gmail.com>
2023-07-15 02:16:53 +0900

    feat: 임시 커밋 입니다.

---
+++ src/lib/utils/api/plants/getPlants.ts
@@ -0,0 +1,18 @@
+import API_BASE_URL from '@/lib/constants/api';
+
+const getPlants = async (plantId: string) => {
+  const response = await fetch(`${API_BASE_URL}/plants`, {
+    method: 'GET',
+    body: JSON.stringify({
+      plantId,
+    }),
+  });
+
+  if (!response.ok) {
+    console.log(response.statusText);
+  }
+
+  return response;
+};
+
+export default getPlants;
---
+++ src/lib/utils/api/recipe/getRecipe.ts
@@ -0,0 +1,15 @@
+import API_BASE_URL from '@/lib/constants/api';
+
+const getRecipe = async () => {
+  const response = await fetch(`${API_BASE_URL}/recipe`, {
+    method: 'GET',
+  });
+
+  if (!response.ok) {
+    console.log(response.statusText);
+  }
+
+  return response;
+};
+
+export default getRecipe;
